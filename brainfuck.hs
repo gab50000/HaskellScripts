f5e86a17bc3564a911c66b6bb31c095cbb6414e1
Gabriel Kabbe <gabriel.kabbe@mail.de>
2022-02-27 16:14:42 +0100

    Update

--- brainfuck.hs
+++ brainfuck.hs
@@ -1,5 +1,9 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-12.21 script
+{-# LANGUAGE BlockArguments #-}
+
+import           Control.Monad (when)
+import           Data.Char     (chr, ord)
 
 
 data BrainfuckOps =
@@ -12,33 +16,53 @@ data BrainfuckOps =
     | JumpForward
     | JumpBack deriving (Show)
 
-type State = [Int]
+data Tape a = Tape [a] a [a] deriving (Show)
 
-initCells::State
-initCells = init 30000
-    where init::Int -> State
-          init n
-              | n == 0 = []
-              | otherwise = 0 : init (n-1)
+type State = Tape Int
+
+-- initCells::State
+-- initCells = replicate 30000 0
 
 type Pos = Int
 
-executeCode::[BrainfuckOps] -> Pos -> State -> IO ()
-executeCode [] pos state = return()
-executeCode (MoveRight:ops) pos state
-    | pos < length state = executeCode ops (pos + 1) state
-    | otherwise = return()
-executeCode (op:ops) pos state = case op of
-    Increment   -> executeCode ops pos (incrementAt state pos 1)
-    Decrement   -> executeCode ops pos (incrementAt state pos (-1))
-    JumpForward -> undefined
-    JumpBack    -> undefined
-    MoveLeft    -> executeCode ops (pos - 1) state
-    MoveRight   ->  executeCode ops (pos + 1) state
-    Output      -> undefined
-    Input       -> undefined
+data Direction = Forward | Backward
 
-    where incrementAt::State->Pos->Int->State
-          incrementAt [] _ _       = []
-          incrementAt (x:xs) 0 val = (x+val):xs
-          incrementAt (x:xs) n val = x : incrementAt xs (n-1) val
+advance :: Tape BrainfuckOps -> Direction -> Maybe(Tape BrainfuckOps)
+advance (Tape _ _ []) Forward          = Nothing
+advance (Tape [] _ _) Backward         = Nothing
+advance (Tape l pivot (r:rs)) Forward  = Just(Tape (pivot:l) r rs)
+advance (Tape (l:ls) pivot r) Backward = Just(Tape ls l (pivot:r))
+
+increase:: State -> Int -> State
+increase (Tape left cell right) val =  Tape left (cell+val) right
+
+execCell :: BrainfuckOps -> State -> IO State
+execCell Increment state                    = return (increase state 1)
+execCell Decrement state                    = return (increase state $ -1)
+execCell MoveLeft state                     = return state
+execCell MoveRight state                    = return state
+execCell Output state@(Tape _ cell _)       = (print . chr) cell >> return state
+execCell Input state@(Tape left cell right) =  do
+                                                char <- getChar
+                                                return $ Tape left (ord char) right
+execCell JumpForward state                  = return state
+execCell JumpBack state                     = return state
+
+executeCode::Maybe (Tape BrainfuckOps) -> State -> IO ()
+executeCode Nothing                _           = return ()
+executeCode (Just tape@(Tape _ Increment _)) state = executeCode (advance tape Forward) (increase state 1)
+executeCode (Just tape@(Tape _ Decrement _)) state = executeCode (advance tape Forward) (increase state $ -1)
+executeCode (Just tape@(Tape _ MoveLeft _)) state = undefined
+executeCode (Just tape@(Tape _ MoveRight _)) state = undefined
+executeCode (Just tape@(Tape _ Output _)) state = undefined
+executeCode (Just tape@(Tape _ Input _)) state = undefined
+executeCode (Just tape@(Tape _ JumpForward _)) state = undefined
+executeCode (Just tape@(Tape _ JumpBack _)) state = undefined
+-- executeCode tape
+    -- MoveLeft  -> when (pos > 0) $ executeCode tape (pos - 1) state
+    -- MoveRight -> when (pos > 0) $ executeCode tape (pos - 1) state
+    -- Output    -> undefined
+    -- Input     -> undefined
+
+-- executeCode tape@(Tape left pivot (r:rs)) = executeCode (Tape pivot:left r rs)  state
+
